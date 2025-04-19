e99c84dda991df991d8d9878c5a973a63969ce18
Hanbabang <10832509+hanbabang@user.noreply.gitee.com>
2025-04-19 21:10:22 +0800

    add await test

--- shared/ui/logo_sequence.gd
+++ shared/ui/logo_sequence.gd
@@ -15,83 +15,90 @@ var showing_first_logo: bool = true
 @onready var color_gray_logo: VBoxContainer = % ColorGrayLogoBlock
 @onready var playstack_logo: VBoxContainer = % PlaystackLogoBlock
 
-func _ready() -> void :
-    grab_focus()
-    color_gray_logo.modulate = invisible_color
-    playstack_logo.modulate = invisible_color
-    active_element = color_gray_logo
-    fade_up_logo(active_element, animation_length)
+func test_coroutine() -> void:
+	print("协程开始")
+	await get_tree().create_timer(1.0).timeout
+	print("协程恢复")
 
+func _ready() -> void :
+	grab_focus()
+	#测试协程的使用，类似UE的定时器
+	#test_coroutine()
+	#print("主线程继续执行")
+	color_gray_logo.modulate = invisible_color
+	playstack_logo.modulate = invisible_color
+	active_element = color_gray_logo
+	fade_up_logo(active_element, animation_length)
 
 func _input(event: InputEvent) -> void :
-    if GamepadUtils.handle_confirm(self, _force_exit):
-        return
+	if GamepadUtils.handle_confirm(self, _force_exit):
+		return
 
-    if GamepadUtils.handle_back(self, _force_exit):
-        return
+	if GamepadUtils.handle_back(self, _force_exit):
+		return
 
-    if not event is InputEventMouseButton:
-        return
+	if not event is InputEventMouseButton:
+		return
 
-    if not event.button_index == MOUSE_BUTTON_LEFT:
-        return
+	if not event.button_index == MOUSE_BUTTON_LEFT:
+		return
 
-    if not event.is_pressed():
-        return
+	if not event.is_pressed():
+		return
 
-    if showing_first_logo:
-        color_gray_logo.visible = false
-        active_element = playstack_logo
-        fade_up_logo(active_element, animation_length)
-        showing_first_logo = false
-    else:
-        _force_exit()
+	if showing_first_logo:
+		color_gray_logo.visible = false
+		active_element = playstack_logo
+		fade_up_logo(active_element, animation_length)
+		showing_first_logo = false
+	else:
+		_force_exit()
 
 
 
 func _force_exit() -> void :
 
-    if leaving_scene:
-        return
-    leaving_scene = true
-    fade_out_logo(active_element, animation_length / 2)
+	if leaving_scene:
+		return
+	leaving_scene = true
+	fade_out_logo(active_element, animation_length / 2)
 
 
 func fade_up_logo(element: Control, length: float) -> void :
-    tween = get_tree().create_tween()
-    tween.tween_property(element, "modulate", full_color, length)
-    tween.finished.connect(_on_fadeup_done.bind(element))
+	tween = get_tree().create_tween()
+	tween.tween_property(element, "modulate", full_color, length)
+	tween.finished.connect(_on_fadeup_done.bind(element))
 
 
 func fade_out_logo(element: Control, length: float) -> void :
-    tween = get_tree().create_tween()
-    tween.tween_property(element, "modulate", invisible_color, length)
-    tween.finished.connect(_on_fadeout_done)
+	tween = get_tree().create_tween()
+	tween.tween_property(element, "modulate", invisible_color, length)
+	tween.finished.connect(_on_fadeout_done)
 
 
 func _on_fadeup_done(element: Control) -> void :
-    await get_tree().create_timer(wait_length).timeout
+	await get_tree().create_timer(wait_length).timeout
 
-    if element == color_gray_logo:
-        active_element = color_gray_logo
-    else:
-        active_element = playstack_logo
-        leaving_scene = true
-    fade_out_logo(active_element, animation_length)
+	if element == color_gray_logo:
+		active_element = color_gray_logo
+	else:
+		active_element = playstack_logo
+		leaving_scene = true
+	fade_out_logo(active_element, animation_length)
 
 
 func _on_fadeout_done() -> void :
-    if is_queued_for_deletion():
-        return
+	if is_queued_for_deletion():
+		return
 
-    if leaving_scene:
-        tween.kill()
-        var splash_screen: Node = splash_screen_packed.instantiate()
-        get_tree().root.add_child(splash_screen)
-        queue_free()
-        return
+	if leaving_scene:
+		tween.kill()
+		var splash_screen: Node = splash_screen_packed.instantiate()
+		get_tree().root.add_child(splash_screen)
+		queue_free()
+		return
 
-    await get_tree().create_timer(wait_length).timeout
-    showing_first_logo = false
-    active_element = playstack_logo
-    fade_up_logo(active_element, animation_length)
+	await get_tree().create_timer(wait_length).timeout
+	showing_first_logo = false
+	active_element = playstack_logo
+	fade_up_logo(active_element, animation_length)
--- shared/utils/gamepad.gd
+++ shared/utils/gamepad.gd
@@ -17,58 +17,58 @@ const ACTION_SHOW_QUICK_NAVIGATOR: StringName = &"show_quick_navigator"
 
 
 static func handle_action(control: Control, handler: Callable, action: StringName) -> bool:
-    if not Input.is_action_just_pressed(action) or not control.has_focus():
-        return false
-    control.get_viewport().set_input_as_handled()
-    handler.call()
+	if not Input.is_action_just_pressed(action) or not control.has_focus():
+		return false
+	control.get_viewport().set_input_as_handled()
+	handler.call()
 
-    return true
+	return true
 
 
 static func back_pressed() -> bool:
-    return Input.is_action_just_pressed(ACTION_BACK)
+	return Input.is_action_just_pressed(ACTION_BACK)
 
 
 static func accept_pressed() -> bool:
-    return Input.is_action_just_pressed(ACTION_CONFIRM)
+	return Input.is_action_just_pressed(ACTION_CONFIRM)
 
 
 static func handle_confirm(control: Control, handler: Callable) -> bool:
-    return handle_action(control, handler, ACTION_CONFIRM)
+	return handle_action(control, handler, ACTION_CONFIRM)
 
 
 static func handle_back(control: Control, handler: Callable) -> bool:
-    return handle_action(control, handler, ACTION_BACK)
+	return handle_action(control, handler, ACTION_BACK)
 
 
 
 static func reroute_neigbor_focus(old_node: Control, new_node: Control) -> void :
-    var new_path: NodePath = new_node.get_path()
-    var opposite_props: Array[Array] = [
-        ["focus_neighbor_top", "focus_neighbor_bottom"], 
-        ["focus_neighbor_bottom", "focus_neighbor_top"], 
-        ["focus_neighbor_left", "focus_neighbor_right"], 
-        ["focus_neighbor_right", "focus_neighbor_left"], 
-    ]
+	var new_path: NodePath = new_node.get_path()
+	var opposite_props: Array[Array] = [
+		["focus_neighbor_top", "focus_neighbor_bottom"], 
+		["focus_neighbor_bottom", "focus_neighbor_top"], 
+		["focus_neighbor_left", "focus_neighbor_right"], 
+		["focus_neighbor_right", "focus_neighbor_left"], 
+	]
 
-    for prop_pair: Array in opposite_props:
-
-
-        var prop: StringName = StringName(prop_pair[0] as String)
-        var opposite_prop: StringName = StringName(prop_pair[1] as String)
-        var path: NodePath = old_node.get(prop) as NodePath
-
-        if path.is_empty():
-            continue
-
-        if not old_node.has_node(path):
+	for prop_pair: Array in opposite_props:
 
 
+		var prop: StringName = StringName(prop_pair[0] as String)
+		var opposite_prop: StringName = StringName(prop_pair[1] as String)
+		var path: NodePath = old_node.get(prop) as NodePath
+
+		if path.is_empty():
+			continue
+
+		if not old_node.has_node(path):
 
 
 
 
 
 
-            continue
-        (old_node.get_node(path) as Control).set(opposite_prop, new_path)
+
+
+			continue
+		(old_node.get_node(path) as Control).set(opposite_prop, new_path)
